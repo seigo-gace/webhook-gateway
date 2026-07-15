0abeabe2af9fe3432be4d8b15ef344bfd1419977
Seigo <216270240+seigo-gace@users.noreply.github.com>
2026-07-16 06:44:28 +0900

    Add test setup

---
+++ test/setup.ts
@@ -0,0 +1,3 @@
+process.env.ADMIN_TOKEN ??= 'test_admin_token_012345678901234567890123456789';
+process.env.DATABASE_URL ??= 'postgres://webhook:webhook_password@127.0.0.1:5432/webhook_gateway';
+process.env.REDIS_URL ??= 'redis://127.0.0.1:6379/0';
